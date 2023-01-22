2776a81b48f39207f92b68b2925eb0a1282126f6
Pavel Shutov <pavelshutoof@gmail.com>
2023-01-22 14:53:33 +0300

    Homework Task2

---
+++ src/ls/task2/First.java
@@ -0,0 +1,165 @@
+package ls.task2;
+
+public class First {
+    //global
+
+    byte bt1;
+    short sh1;
+    int in1;
+    long lg1;
+    float fl1;
+    double dbl1;
+    char ch1;
+    boolean bl1;
+
+    Byte byt1;
+    Short shr1;
+    Integer number1;
+    Long lon1;
+    Float flt1;
+    Double db1;
+    Character chart1;
+    Boolean bool1;
+
+    public static void main(String[] args) {
+        //local
+
+        byte bt2 = 2;
+        short sh2 = 23;
+        int in2 = 343;
+        long lg2 = 3456L;
+        float fl2 = 34.5F;
+        double dbl2 = 3456.45;
+        char ch2 = '3';
+        boolean bl2 = true;
+
+        byte bt3;
+        short sh3;
+        int in3;
+        int num;
+        long lg3;
+        float fl3;
+        double dbl3;
+        char ch3;
+        boolean bl3;
+
+        //Экспиремент с присвоением переменных друг в друга
+
+        sh3 = bt2;
+        System.out.println("присвоение byte в shot =  " + sh3);
+
+        in3 = sh2;
+        System.out.println("присвоение shot в int = " + in3);
+
+        lg3 = in2;
+        System.out.println("присвоение int в long = " + lg3);
+
+        num = ch2;
+        System.out.println("присвоение char в int = " + num);
+
+        dbl3 = fl2;
+        System.out.println("присвоение float в double = " + dbl3);
+
+        System.out.println("****************"); //разделение строк
+
+
+
+
+
+
+
+
+
+        Byte byt1;
+        Short shr1;
+        Integer number1;
+        Long lon1;
+        Float flt1;
+        Double db1;
+        Character chart1;
+        Boolean bool1;
+
+        //создал экзепляр класса Second, иницилизировал и вывел переменные
+
+        Second second = new Second();
+
+        second.setBt((byte)(10));
+        System.out.println(second.getBt());
+
+        second.setSh((short)(12));
+        System.out.println(second.getSh());
+
+        second.setIn(1234);
+        System.out.println(second.getIn());
+
+        second.setCh((char)(88));
+        System.out.println(second.getCh());
+
+        second.setFl(3.14F);
+        System.out.println(second.getFl());
+
+        second.setDbl(12.222);
+        System.out.println(second.getDbl());
+
+        second.setLg(9223372036854775807L);
+        System.out.println(second.getLg());
+
+        second.setBl(false);
+        System.out.println(second.isBl());
+
+        System.out.println("****************"); //разделение строк между классами
+
+
+        // create new class
+
+        Second second2 = new Second();
+
+        second2.setByt((byte)(23));
+        System.out.println(second2.getByt());
+
+        second2.setShr((short)(58));
+        System.out.println(second2.getShr());
+
+        second2.setNumber(2023);
+        System.out.println(second2.getNumber());
+
+        second2.setChart((char)(89));
+        System.out.println(second2.getChart());
+
+        second2.setLon(45585858L);
+        System.out.println(second2.getLon());
+
+        second2.setFlt(23.34F);
+        System.out.println(second2.getFlt());
+
+        second2.setDb(234.43);
+        System.out.println(second2.getDb());
+
+        second2.setBool(true);
+        System.out.println(second2.getBool());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ src/ls/task2/Second.java
@@ -0,0 +1,162 @@
+package ls.task2;
+
+public class Second {
+    //примитивы
+
+
+    private byte bt;
+    private short sh;
+    private int in;
+    private long lg;
+    private float fl;
+    private double dbl;
+    private char ch;
+    private boolean bl;
+
+    // обертки
+
+
+    private Byte byt;
+    private Short shr;
+    private Integer number;
+    private Long lon;
+    private Float flt;
+    private Double db;
+    private Character chart;
+    private Boolean bool;
+
+    //getter and setter для примитивов
+
+
+    public byte getBt() {
+        return bt;
+    }
+
+    public void setBt(byte bt) {
+        this.bt = bt;
+    }
+
+    public short getSh() {
+        return sh;
+    }
+
+    public void setSh(short sh) {
+        this.sh = sh;
+    }
+
+    public int getIn() {
+        return in;
+    }
+
+    public void setIn(int in) {
+        this.in = in;
+    }
+
+    public long getLg() {
+        return lg;
+    }
+
+    public void setLg(long lg) {
+        this.lg = lg;
+    }
+
+    public float getFl() {
+        return fl;
+    }
+
+    public void setFl(float fl) {
+        this.fl = fl;
+    }
+
+    public double getDbl() {
+        return dbl;
+    }
+
+    public void setDbl(double dbl) {
+        this.dbl = dbl;
+    }
+
+    public char getCh() {
+        return ch;
+    }
+
+    public void setCh(char ch) {
+        this.ch = ch;
+    }
+
+    public boolean isBl() {
+        return bl;
+    }
+
+    public void setBl(boolean bl) {
+        this.bl = bl;
+    }
+
+    //getter and setter для оберток
+
+
+    public Byte getByt() {
+        return byt;
+    }
+
+    public void setByt(Byte byt) {
+        this.byt = byt;
+    }
+
+    public Short getShr() {
+        return shr;
+    }
+
+    public void setShr(Short shr) {
+        this.shr = shr;
+    }
+
+    public Integer getNumber() {
+        return number;
+    }
+
+    public void setNumber(Integer number) {
+        this.number = number;
+    }
+
+    public Long getLon() {
+        return lon;
+    }
+
+    public void setLon(Long lon) {
+        this.lon = lon;
+    }
+
+    public Float getFlt() {
+        return flt;
+    }
+
+    public void setFlt(Float flt) {
+        this.flt = flt;
+    }
+
+    public Double getDb() {
+        return db;
+    }
+
+    public void setDb(Double db) {
+        this.db = db;
+    }
+
+    public Character getChart() {
+        return chart;
+    }
+
+    public void setChart(Character chart) {
+        this.chart = chart;
+    }
+
+    public Boolean getBool() {
+        return bool;
+    }
+
+    public void setBool(Boolean bool) {
+        this.bool = bool;
+    }
+
+}

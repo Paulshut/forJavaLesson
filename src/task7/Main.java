7a8d2aebf813e4f6fdbd41e3c24a365e40c325ad
Pavel Shutov <pavelshutoof@gmail.com>
2023-02-13 17:53:51 +0300

    Homework task7

--- src/task7/Main.java
+++ src/task7/Main.java
@@ -3,5 +3,6 @@ package task7;
 public class Main {
     public static void main(String[] args) {
        Start.showRobots();
+
     }
 }
--- src/task7/Robot.java
+++ src/task7/Robot.java
@@ -1,16 +1,17 @@
 package task7;
 
+import java.util.Random;
+
 public class Robot {
     private Head head;
     private Body body;
     private String nameRobot;
     private String nameBody;
     private String nameHead;
-    private Body[] bodies = {Body.BENDER_BODY, Body.OPTUMUS_BODY, Body.ROBOCOP_BODY};
-    private Head[] heads = {Head.BENDER_HEAD, Head.OPTUMUS_HEAD, Head.ROBOCOP_HEAD};
+    private Body[] bodies2 = new Body[2];
 
-    public Robot(String nameRobot) {
-        this.nameRobot = nameRobot;
+    public Robot() {
+        this.nameRobot = getNameRobot();
         this.head = new Factory().createHead();
         this.body = new Factory().createBody();
         this.nameBody = body.getNameBody();
@@ -20,16 +21,32 @@ public class Robot {
     private class Factory implements task7.Factory {
         @Override
         public Body createBody() {
+            Body[] bodies = {Body.BENDER_BODY, Body.OPTUMUS_BODY, Body.ROBOCOP_BODY};
             int i = (int) (Math.random() * 3);
             return body = bodies[i];
         }
 
         @Override
         public Head createHead() {
+            Head[] heads = {Head.BENDER_HEAD, Head.OPTUMUS_HEAD, Head.ROBOCOP_HEAD};
             int i = (int) (Math.random() * 3);
             return head = heads[i];
         }
 
+    }
+
+    private String getNameRobot() {
+        int l = 1;
+        char ch;
+        String nameRobot;
+        Random random = new Random();
+        nameRobot = String.valueOf((char) (random.nextInt(26) + 'A'));
+        while (l < 5) {
+            ch = (char) (random.nextInt(26) + 'a');
+            nameRobot = nameRobot + ch;
+            l++;
+        }
+        return nameRobot;
     }
 
     @Override
